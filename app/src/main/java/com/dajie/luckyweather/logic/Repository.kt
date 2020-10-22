7a268f5577864bdf1d49bd6abfa28008891de189
fuyajie <fuyajie@1yd.me>
2020-10-22 14:52:55 +0800

    下拉刷新，切换城市,logo

--- app/src/main/java/com/dajie/luckyweather/logic/Repository.kt
+++ app/src/main/java/com/dajie/luckyweather/logic/Repository.kt
@@ -38,6 +38,13 @@ object Repository {
         emit(result as Result<*>)
     }
 
+//    kotlin异步的一般步骤
+//    async关键字创建协程；
+//    await 挂起函数，启动协程,至少要有一个挂起函数。挂起函数只能在协程/挂起函数中被调用，不能在普通函数里调用。
+//    协程与线程的区别
+//    协程是通过编译技术实现(不需要虚拟机VM/操作系统OS的支持)，通过插入相关代码来生效。与之相反，线程/进程是需要虚拟机VM/操作系统OS的支持，通过调度CPU执行生效。
+//    线程阻塞的代价昂贵，尤其在高负载时的可用线程很少，阻塞线程会导致一些重要任务缺少可用线程而被延迟。协程挂起几乎无代价，无需上下文切换或涉及OS。
+
     fun refreshWeather(lng: String, lat: String) = liveData(Dispatchers.IO) {
         val result = try {
             coroutineScope {
